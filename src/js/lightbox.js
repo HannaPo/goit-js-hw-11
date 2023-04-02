91c791ffb3f9d8e009bd2f4b357c8dd6a92081de
HannaPo <114150796+HannaPo@users.noreply.github.com>
2023-04-02 17:47:33 +0300

    lightbox

--- src/js/lightbox.js
+++ src/js/lightbox.js
@@ -1,7 +1,7 @@
 import SimpleLightbox from "simplelightbox";
 import "simplelightbox/dist/simple-lightbox.min.css";
 
-let lightbox = new SimpleLightbox('.gallery__item', {
+let lightbox = new SimpleLightbox('.gallery a', {
   captionsData: 'alt',
   captionDelay: 250,
 });
